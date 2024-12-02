08a52799f808a7c273b665e217679697f3c576a9
codeyard1 <codeyard1@gmail.com>
2024-12-02 15:00:34 +0300

    final

---
+++ lib/app_config/app_config.dart
@@ -0,0 +1,20 @@
+class AppConfig {
+  static const String appName = 'PokerKing';
+  static const String packageName = 'com.flutter.cryptopoker';
+  static const String languageDefault = "en";
+  static const String defaultTheme = 'light';
+  static const bool isDemoMode = true;
+  static final Map<String, String> languagesSupported = {
+    'en': "English",
+    'ar': "عربى",
+    'pt': "Portugal",
+    'fr': "Français",
+    'id': "Bahasa Indonesia",
+    'es': "Español",
+    'it': "italiano",
+    'tr': "Türk",
+    'sw': "Kiswahili",
+    'ro': "Română",
+    'de': "Deutsch",
+  };
+}
